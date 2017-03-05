8abc34f6d47b3e0e6c5232d60f05ea19eb094f42
Ualison Aguiar <ualison.aguiar@gmail.com>
2017-03-05 16:33:11 -0300

    Criação do cliente

--- index.php
+++ index.php
@@ -8,12 +8,13 @@
 <body>
 <div class="container">
     <h2>Client Rest</h2>
+
     <div>
         <div class="row">
             <div class="col-md-6">
                 <div class="form-group">
-                    <label for="urlServico">URL do serviço rest:</label>
-                    <input type="text" class="form-control" id="urlServico" placeholder="URL do serviço rest"
+                    <label for="urlServidor">URL do sistema rest:</label>
+                    <input type="text" class="form-control" id="urlServidor" placeholder="URL do serviço rest"
                            value="http://usuario-rest.local">
                 </div>
             </div>
@@ -24,6 +25,7 @@
         <div class="row">
             <div class="col-md-6">
                 <h3>Solicitar Token</h3>
+
                 <div class="form-group">
                     <label for="usuario">Usuário:</label>
                     <input type="text" class="form-control" id="usuario" placeholder="Informe usuário">
@@ -32,31 +34,75 @@
                     <label for="pwd">Senha:</label>
                     <input type="password" class="form-control" id="pwd" placeholder="Informe a senha">
                 </div>
+                <div class="form-group">
+                    <button id="solicitarToken" type="button" class="btn btn-primary" value="Solicitar Token">Solicitar
+                        Token
+                    </button>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <h3>Envio da requisição</h3>
+
+                <div class="form-group">
+                    <textarea id="envioRequisicao" class="form-control"
+                              style="margin: 0px -25px 0px 0px; height: 168px; width: 579px;"></textarea>
+                </div>
             </div>
             <div class="col-md-6">
                 <h3>Resultado da requisição</h3>
+
                 <div class="form-group">
                     <textarea id="resultadoToken" class="form-control"
                               style="margin: 0px -25px 0px 0px; height: 168px; width: 579px;"></textarea>
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="col-md-6">
-                <button id="solicitarToken" type="button" class="btn btn-primary" value="Solicitar Token">Solicitar
-                    Token
-                </button>
-            </div>
-        </div>
     </div>
     <hr>
     <div>
         <h3>CRUD de usuário</h3>
+
         <div class="row">
             <div class="col-md-6">
                 <div class="form-group">
                     <label for="usuario">Token:</label>
                     <input type="text" class="form-control" id="token" placeholder="Informe o token">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label for="Metodo">Metodo:</label>
+                    <select name="metodo" id="metodo" class="form-control">
+                        <option value="">Selecione</option>
+                        <option value="GET">GET</option>
+                        <option value="POST">POST</option>
+                        <option value="PUT">PUT</option>
+                        <option value="DELETE">DELETE</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <label for="Metodo">Url do serviço:</label>
+                <input type="text" class="form-control" name="urlServico" id="urlServico">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <button id="enviaRquisicao" type="button" class="btn btn-primary" value="Enviar">Enviar
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6" style="margin-top: -254px;margin-left: 590px;">
+                <h3>Dados de Envio</h3>
+
+                <div class="form-group">
+                    <textarea id="textoRequisicao" class="form-control"
+                              style="margin: 0px -25px 0px 0px; height: 168px; width: 579px;"></textarea>
                 </div>
             </div>
         </div>
@@ -72,29 +118,59 @@
                     usuario: $('#usuario').val(),
                     senha: $('#pwd').val()
                 };
-                getRequisicao('solicitar-token', 'POST', arrData, 'application/json');
+                getRequisicao('solicitar-token', 'POST', JSON.stringify(arrData), 'application/json');
             }
         });
 
-        function getRequisicao(strUrl, strMethod, arrData, strContentType) {
+        $('#enviaRquisicao').click(function() {
+            var strContentType = '';
+            if ($('#metodo').val() !== 'GET') {
+                strContentType = 'application/json';
+            }
+            var arrHeader = {
+                Authorization: 'Bearer ' + $('#token').val(),
+            };
+            getRequisicao($('#urlServico').val(), $('#metodo').val(), $('#textoRequisicao').val(), strContentType, arrHeader);
+        });
+
+        function getRequisicao(strUrl, strMethod, arrData, strContentType, arrHeader) {
             if (strContentType == '') {
                 strContentType = null;
             }
-            var strUrlServico = $('#urlServico').val();
+            if (arrHeader == '') {
+                arrHeader = [];
+            }
+            var strUrlServico = $('#urlServidor').val();
             if (strUrlServico === '') {
                 alert('Informa a url do serviço rest');
                 return false;
             } else {
+                strUrl = strUrlServico + '/' + strUrl;
                 $.ajax({
-                    url: strUrlServico + '/' + strUrl,
+                    url: strUrl,
                     type: strMethod,
                     data: arrData,
                     contentType: strContentType,
+                    headers: arrHeader,
                     success: function (result) {
                         $('#resultadoToken').html(JSON.stringify(result, null, 2));
                     }
                 });
+                exibeDetalheRequiscao(strMethod, strUrl, arrHeader, arrData);
             }
+        }
+
+        function exibeDetalheRequiscao(strMethod, strUrl, arrHeader, arrData)
+        {
+            var strEnvio = 'Metodo: ' + strMethod + '\n';
+            strEnvio += 'URL: ' + strUrl + '\n';
+            if (arrHeader) {
+                strEnvio += 'Header:\n';
+                strEnvio += JSON.stringify(arrHeader, null, 2);
+            }
+            strEnvio += '\nData \n';
+            strEnvio += arrData;
+            $('#envioRequisicao').html(strEnvio);
         }
     });
 </script>
